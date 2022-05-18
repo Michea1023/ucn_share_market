b14c8fd7b4d587dd412ef9112e76aa2e73a61303
pipeta <pipe.tapia.guerra1@gmail.com>
2022-05-18 15:28:00 -0400

    Login and Register

--- bolsasantiago/src/App.js
+++ bolsasantiago/src/App.js
@@ -3,10 +3,12 @@ import React from "react";
 
 import UserProfile from './Pages/UserProfile';
 import HomePage from './Pages/HomePage';
+import Login from './Pages/Login';
+import Register from './Pages/Register';
  
  export default function App(){
   return(
-    <HomePage />
+    < Login/>
     
   );
 }
