45db0ae3e010d7542f1dd7c885116b0e9f01c55a
pipeta <pipe.tapia.guerra1@gmail.com>
2022-06-02 19:09:17 -0400

    Avance felipe

--- frontend/src/components/NavBar/BarraPrincipal.js
+++ frontend/src/components/NavBar/BarraPrincipal.js
@@ -1,26 +1,103 @@
-import React from "react";
+import React, {Component} from "react";
+import {scryRenderedComponentsWithType} from "react-dom/test-utils";
+
 //import logo from "../../../static/images/logo.png";
 //import cierre_sesion from "../../../static/images/logo-cierre-sesion.png"
+//este iba en el primer div linea 20
+//<img className="navbar-img" src= {logo}></img>
+//este iba en el div linea 28
+//<img className="navbar-img cierre" src={cierre_sesion}></img>
+//import {Gridcontainer, griditem1, griditem3 ,griditem2,navbarusuario,navbartitle} from "./PrincipalElements"
 
-export default function BarraPrincipal() {
-    return (
-        <header className="grid-container">
-            <div className="grid-item1">
-                <img className="navbar-img" src= {logo}></img>
-            </div>
-            <div className="grid-item2">
-                <h2 className="navbar-title">Bolsa de Santiago</h2>
-            </div>
-            <div className="grid-item3">
-                <div className="navbar-usuario">
+
+import styled from "styled-components";
+
+const Gridcontainer = styled.header`
+  display: grid;
+  grid-template-columns: 1fr 2fr 1fr;
+  grid-template-rows: 60px;
+  background-color: #A7CDD9;
+  height: 60px;
+  box-shadow: 0px 3px 12px rgba(0, 0, 0, 0.252);
+
+`;
+
+ const Griditem1 = styled.header`
+  grid-row: 1;
+
+
+`;
+ const Griditem2 = styled.div`
+  grid-row: 1;
+  grid-column: 2;
+  display: flex;
+  justify-content:center;
+  align-items:center;
+
+`;
+
+ const Navbartitle = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 2fr 1fr;
+  grid-template-rows: 60px;
+
+
+`;
+
+ const Griditem3 = styled.div`
+  grid-row: 1;
+  grid-column: 3;
+  display:flex;
+  justify-content:flex-end;
+  align-items:center;
+
+
+`;
+
+ const Navbarusuario = styled.div`
+  display: flex;
+   
+  p{
+    &:nth-child(1)={
+        font-size: 1.5rem;
+        font-weight: 500;
+    }
+  
+  
+  }
+
+`;
+
+
+
+export default class BarraPrincipal extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render(){
+
+        return (
+        <Gridcontainer>
+            <Griditem1>
+
+            </Griditem1>
+            <Griditem2>
+                <Navbartitle>Bolsa de Santiago</Navbartitle>
+            </Griditem2>
+            <Griditem3>
+                <Navbarusuario>
                     <p>NOMBRE DE USUARIO</p>
-                    <img className="navbar-img cierre" src={cierre_sesion}></img>
 
-                </div>
-            </div>
-            
-        </header>
+
+                </Navbarusuario>
+            </Griditem3>
+
+        </Gridcontainer>
     )
+
+    }
+
 }
     
 
